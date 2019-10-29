fe59d3b762b2b0ea3c278d8f1c53995d0c8fe08d
mehrbod khiabani <mehrbodkhiabani@yahoo.com>
2019-10-29 12:50:38 +0330

    feat: Adds the preview of login screen.

--- app/src/main/java/com/mehrbod/digipaycodechallenge/MainActivity.kt
+++ app/src/main/java/com/mehrbod/digipaycodechallenge/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.mehrbod.digipaycodechallenge
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mehrbod.digipaycodechallenge.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val SHARED_PREFERENCES_NAME = "DIGIPAY_CODE_CHALLENGE"
+        const val IS_LOGGED_IN = "is_logged_in_already"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+        openNeededActivity(sharedPreferences)
+    }
+
+    private fun openNeededActivity(preferences: SharedPreferences) {
+        preferences.getBoolean(IS_LOGGED_IN, false).let { isLoggedIn ->
+            if (isLoggedIn) {
+                openTrackActivity()
+            } else {
+                openLoginActivity()
+            }
+        }
+    }
+
+    private fun openTrackActivity() {
+
+    }
+
+    private fun openLoginActivity() {
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
